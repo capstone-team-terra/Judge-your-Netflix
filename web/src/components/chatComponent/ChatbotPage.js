848bbbfdc01c4b672cc5791021a7286207a43077
yvonneshui <yvonne.shui126@gmail.com>
2021-03-22 20:34:03 -0400

    FE: chatbot rendering options

--- web/src/components/chatComponent/ChatbotPage.js
+++ web/src/components/chatComponent/ChatbotPage.js
@@ -1,18 +1,19 @@
 import React from "react";
 import AllResults from "../resultsComponent/AllResults";
 import ChatBot from "react-simple-chatbot";
-const steps = [
-  {
-    id: "0",
-    message: "We judge your Netflix taste.",
-    trigger: "1",
-  },
-  {
-    id: "1",
-    message: "Bye!",
-    end: true,
-  },
-];
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  background: "#282c34",
+  fontFamily: "Helvetica Neue",
+  headerBgColor: "#282c34",
+  headerFontColor: "#282c34",
+  headerFontSize: "15px",
+  botBubbleColor: "#282c34",
+  botFontColor: "#fff",
+  userBubbleColor: "#282c34",
+  userFontColor: "#4a4a4a",
+};
 class ChatbotPage extends React.Component {
   constructor(props) {
     super(props);
@@ -28,13 +29,53 @@ class ChatbotPage extends React.Component {
   handleClick() {
     this.setState({ clicked: true });
   }
+
   render() {
+    const genresCount = this.props.result.genres;
+    const resultsArr = Object.entries(genresCount);
+    const sorted = resultsArr.sort((a, b) => b[1] - a[1]);
+    const steps = [
+      {
+        id: "1",
+        message: "Are you ready for some serious roasting time?",
+        trigger: "2",
+      },
+      {
+        id: "2",
+        options: [{ value: 1, label: "Yes go ahead!", trigger: "3" }],
+      },
+      {
+        id: "3",
+        message: "Okay. Let me see... ",
+        trigger: "4",
+      },
+      {
+        id: "4",
+        message: `Did you really watch ${sorted[0][1]} ${sorted[0][0]} shows?`,
+        trigger: "5",
+      },
+      {
+        id: "5",
+        options: [{ value: 1, label: "I guess so... Why?", trigger: "6" }],
+      },
+      { id: "6", message: "Nothing...", end: true },
+    ];
     return this.state.clicked ? (
       <AllResults result={this.state.result} />
     ) : (
       <div>
         <h2>Rendering Chatbot!</h2>
-        <ChatBot steps={steps} />
+        <ThemeProvider theme={theme}>
+          <ChatBot
+            steps={steps}
+            width="1000px"
+            placeholder=" "
+            hideBotAvatar="true"
+            hideHeader="true"
+            hideUserAvatar="true"
+          />
+        </ThemeProvider>
+
         <button type="button" onClick={this.handleClick}>
           Get Your Results!
         </button>
